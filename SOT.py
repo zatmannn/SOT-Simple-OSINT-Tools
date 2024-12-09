a554226f5fbdf3918d83122908a715065bbaad76
zatman <yaeblannn22222@proton.me>
2024-12-09 22:41:24 +0700

    Update SOT.py

--- SOT.py
+++ SOT.py
@@ -2,7 +2,18 @@ import requests
 from pystyle import Colors, Write, Center
 from phonenumbers import geocoder, carrier, timezone
 import phonenumbers
+import whois
 import os
+from concurrent.futures import ThreadPoolExecutor
+
+default_color = Colors.red  
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def restart():
+    Write.Input("\nPress Enter to return to the main menu...", default_color, interval=0)
+    clear()
 
 def ip_info(ip):
     url = f"https://ipinfo.io/{ip}/json"
@@ -28,22 +39,42 @@ def ip_info(ip):
         Write.Print(Center.XCenter(ip_details), Colors.white, interval=0)
 
     except requests.exceptions.Timeout:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        Write.Print("\n[!] > Request timed out. Please try again later.", Colors.red, interval = 0)
+        clear()
+        Write.Print("\n[!] > Request timed out. Please try again later", default_color, interval = 0)
     except requests.exceptions.ConnectionError:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        Write.Print("\n[!] > Connection error.", Colors.red, interval = 0)
+        clear()
+        Write.Print("\n[!] > Connection error", default_color, interval = 0)
     except requests.exceptions.HTTPError as e:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        Write.Print(f"\n[!] > HTTP error: {e.response.status_code}", Colors.red, interval = 0)
-    except Exception as e:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        Write.Print(f"\n[!] > An error occurred: {e}", Colors.red, interval = 0)
+        clear()
+        Write.Print(f"\n[!] > HTTP error: {e.response.status_code}", default_color, interval = 0)
+    except Exception:
+        clear()
+        Write.Print(f"\n[!] > Error", default_color, interval = 0)
 
-    Write.Input("\nPress Enter to return to the main menu...", Colors.red, interval=0)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    restart()
 
 def username_search(nickname):
+    def check_url(url):
+        try:
+            response = requests.get(url, timeout=10)
+            status_code = response.status_code
+
+            if status_code == 200:
+                return f"[+] > {url:<50}|| Found"
+            elif status_code == 404:
+                return f"[-] > {url:<50}|| Not found"
+            else:
+                return f"[-] > {url:<50}|| Error: {status_code}"
+
+        except requests.exceptions.Timeout:
+            return f"[-] > {url:<50}|| Timeout"
+        except requests.exceptions.ConnectionError:
+            return f"[-] > {url:<50}|| Connection error"
+        except requests.exceptions.RequestException:
+            return f"[-] > {url:<50}|| Request error"
+        except Exception:
+            return f"[-] > {url:<50}|| Unexpected error"
+
     urls = [f"https://www.instagram.com/{nickname}",
             f"https://www.tiktok.com/@{nickname}",
             f"https://www.x.com/{nickname}",
@@ -56,35 +87,24 @@ def username_search(nickname):
             f"https://www.reddit.com/{nickname}",
             f"https://www.github.com/{nickname}",
             f"https://vk.com/{nickname}"]
+
     search_results = f"""
 ╭─{" "*78}─╮
 |{' '*27}Social Media Search Results{' '*26}|
 |{"="*80}|
 """
 
-    for url in urls:
-        try:
-            response = requests.get(url)
-            status_code = response.status_code
-            
-            if status_code == 200:
-                result_text = f"[+] > {url:<50}|| Found"
-            elif status_code == 404:
-                result_text = f"[-] > {url:<50}|| Not found"
-            else:
-                result_text = f"[-] > {url:<50}|| Error: {status_code}"
+    with ThreadPoolExecutor() as executor:
+        results = list(executor.map(check_url, urls))
+    
+    for result in results:
+        search_results += f"| {result:<78} |\n"
 
-            search_results += f"| {result_text:<78} |\n"
-        
-        except requests.exceptions.RequestException as e:
-            result_text = f"[-] > {url:<50} || Error: {str(e)}"
-            search_results += f"| {result_text:<78} |\n"
-
-    search_results += f"╰─{' '*55}─╯╰─{' '*19}─╯"
+    search_results += f"╰─{' '*78}─╯"
 
     Write.Print(Center.XCenter(search_results), Colors.white, interval=0)
-    Write.Input("\nPress Enter to return to the main menu...", Colors.red, interval=0)
-    os.system('cls' if os.name == 'nt' else 'clear')
+
+    restart()
 
 def phone_info(phone_number):
     try:
@@ -93,30 +113,104 @@ def phone_info(phone_number):
         region = geocoder.description_for_number(parsed_number, "en")
         operator = carrier.name_for_number(parsed_number, "en")
         valid = phonenumbers.is_valid_number(parsed_number)
-        validity_text = "Valid" if valid else "Invalid"
+        validity = "Valid" if valid else "Invalid"
         phonetext = f"""
 \n
 ╭─{" "*50}─╮
 |{' '*17}Phone number info{' '*18}|
 |{"="*52}|
-| [+] > Number:   || {phone_number: <32}|
-| [+] > Country:  || {country: <32}|
-| [+] > Regiom:   || {region: <32}|
-| [+] > Operator: || {operator: <32}|
-| [+] > Validity: || {validity_text: <32}|
+| [+] > Number   || {phone_number: <33}|
+| [+] > Country  || {country: <33}|
+| [+] > Regiom   || {region: <33}|
+| [+] > Operator || {operator: <33}|
+| [+] > Validity || {validity: <33}|
 ╰─{" "*15}─╯╰─{" "*31}─╯\n"""
 
         Write.Print(Center.XCenter(phonetext), Colors.white, interval=0)
     
     except phonenumbers.phonenumberutil.NumberParseException:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        Write.Print(f"\n[!] > Error: invalid phone number format", Colors.red, interval = 0)
+        clear()
+        Write.Print(f"\n[!] > Error: invalid phone number format", default_color, interval = 0)
 
-    Write.Input("\nPress Enter to return to the main menu...", Colors.red, interval=0)
-    os.system('cls' if os.name == 'nt' else 'clear')
+    restart()
+
+def domain_info(domain):
+
+    def domain_date(date):
+        if isinstance(date, list):
+            date = date[0]
+        return date.strftime("%Y-%m-%d") if date else "Unknown"
+
+    try:
+        domain_data = whois.whois(domain)
+        domain_name = domain_data.domain_name
+        domain_registrar = domain_data.registrar
+        domain_creation_date = domain_date(domain_data.creation_date)
+        domain_expiration_date = domain_date(domain_data.expiration_date)
+        domain_updated_date = domain_date(domain_data.updated_date)
+        registrant_name = domain_data.registrant_name if domain_data.registrant_name else "Private"
+        registrant_email = domain_data.registrant_email if domain_data.registrant_email else "Private"
+
+        domain_details = f"""
+╭─{" "*78}─╮
+|{' '*34} WHOIS Info {' '*34}|
+|{"="*80}|
+| [+] > Domain:           || {domain_name: <52}|
+| [+] > Registrar:        || {domain_registrar: <52}|
+| [+] > Creation Date:    || {domain_creation_date: <52}|
+| [+] > Expiration Date:  || {domain_expiration_date: <52}|
+| [+] > Last Updated:     || {domain_updated_date: <52}|
+| [+] > Registrant Name:  || {registrant_name: <52}|
+| [+] > Registrant Email: || {registrant_email: <52}|
+╰─{" "*23}─╯╰─{" "*51}─╯
+"""
+        Write.Print(Center.XCenter(domain_details), Colors.white, interval=0)
+    except Exception:
+        Write.Print(f"[!] > Error", default_color, interval=0)
+
+    restart()
+
+def change_color():
+    global default_color
+    clear()
+    color_menu = """
+╭─    ─╮╭─                     ─╮
+|  №   ||         Color         |
+|======||=======================|
+| [1]  || Red                   |
+| [2]  || Blue                  |
+| [3]  || Green                 |
+| [4]  || Yellow                |
+| [5]  || Cyan                  |
+| [6]  || White                 |
+|------||-----------------------|
+| [0]  || Back to settings menu |
+╰─    ─╯╰─                     ─╯
+"""
+    Write.Print(Center.XCenter(color_menu), Colors.white, interval=0)
+
+    color_choice = Write.Input("\n\n[?] >  ", default_color, interval=0).strip()
+
+    color = {"1": Colors.red,
+        "2": Colors.blue,
+        "3": Colors.green,
+        "4": Colors.yellow,
+        "5": Colors.cyan,
+        "6": Colors.white}
+
+    if color_choice in color:
+        default_color = color[color_choice]
+        clear()
+        Write.Print("[!] > Color changed successfully.\n", default_color, interval=0)
+    elif color_choice == "0":
+        settings()
+    else:
+        clear()
+        Write.Print("[!] > Invalid choice. Please choose a number from 1 to 6.\n", Colors.red, interval=0)
+    restart()
 
 def main():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    clear()
     while True:
         menu_art = """\n   ▄████████  ▄██████▄      ███     
   ███    ███ ███    ███ ▀█████████▄ 
@@ -128,60 +222,115 @@ def main():
  ▄████████▀   ▀██████▀     ▄████▀   
 \n"""
 
-        menu = """\n\n                                 [ S.O.T ]\n
-                            Simple OSINT Tools
+        menu = """\n\n                                   [ S.O.T ]\n
+                               Simple OSINT Tools
 
-╭─   ─╮╭─                 ─╮╭─                                             ─╮
-|  №  ||     Function      ||                   Description                 |
-|=====||===================||===============================================|
-| [1] || IP info search    || Search information about an IP address        |
-| [2] || Username search   || Search for profiles on social media platforms |
-| [3] || Phone info search || Search information about a phone number       |
-| [0] || Exit              || Exit the program                              |
-╰─   ─╯╰─                 ─╯╰─                                             ─╯\n\n\n"""
+╭─    ─╮╭─                   ─╮╭─                                             ─╮
+|  №   ||      Function       ||                  Description                  |
+|======||=====================||===============================================|
+| [1]  || IP Info Search      || Search for information about an IP address    |
+| [2]  || Username Search     || Search for profiles on social media platforms |
+| [3]  || Phone Info Search   || Search for information about a phone number   |
+| [4]  || Domain Info Search  || Search for information about a domain         |
+|------||---------------------||-----------------------------------------------|
+| [0]  || Exit                || Exit the program                              |
+| [99] || Settings            || Set up this tool                              |
+╰─    ─╯╰─                   ─╯╰─                                             ─╯\n\n\n"""
 
-        Write.Print(Center.XCenter(menu_art), Colors.red, interval = 0)
+        Write.Print(Center.XCenter(menu_art), default_color, interval = 0)
         Write.Print(Center.XCenter(menu), Colors.white, interval = 0)
 
-        choice = Write.Input("[?] >  ", Colors.red, interval = 0).strip()
+        choice = Write.Input("[?] >  ", default_color, interval = 0).strip()
+
         if choice == "1":
-            os.system('cls' if os.name == 'nt' else 'clear')
-            ip = Write.Input("[?] > IP-Adress: ", Colors.red, interval = 0)
+            clear()
+            ip = Write.Input("[?] > IP-Adress: ", default_color, interval = 0)
             if not ip:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                Write.Print("[!] > Please, enter IP Adress\n", Colors.red, interval = 0)
+                clear()
+                Write.Print("[!] > Please, enter IP Adress\n", default_color, interval = 0)
                 continue
             ip_info(ip)
 
         elif choice == "2":
-            os.system('cls' if os.name == 'nt' else 'clear')
-            nickname = Write.Input("[?] > Username: ", Colors.red, interval = 0)
-            Write.Print("[!] > Please wait, it may take some time...\n", Colors.red, interval = 0)
+            clear()
+            nickname = Write.Input("[?] > Username: ", default_color, interval = 0)
+            Write.Print("[!] > Please wait, it may take some time...\n", default_color, interval = 0)
             if not nickname:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                Write.Print("[!] > Please, enter username\n", Colors.red, interval = 0)
+                clear()
+                Write.Print("[!] > Please, enter username\n", default_color, interval = 0)
                 continue
             username_search(nickname)
 
         elif choice == "3":
-            os.system('cls' if os.name == 'nt' else 'clear')
-            phone_number = Write.Input("[?] > Phone number: ", Colors.red, interval = 0)
+            clear()
+            phone_number = Write.Input("[?] > Phone number: ", default_color, interval = 0)
             if not phone_number:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                Write.Print("[!] > Please, enter phone number\n", Colors.red, interval = 0)
+                clear()
+                Write.Print("[!] > Please, enter phone number\n", default_color, interval = 0)
                 continue
             phone_info(phone_number)
             
         elif choice == "4":
-            os.system('cls' if os.name == 'nt' else 'clear')
-        
+            clear()
+            domain = Write.Input("[?] > URL: ", default_color, interval = 0)
+            Write.Print("[!] > Please wait, it may take some time...\n", default_color, interval = 0)
+            if not domain:
+                clear()
+                Write.Print("[!] > Please, enter URL\n", default_color, interval = 0)
+                continue
+            domain_info(domain)
+
+
         elif choice == "0":
-            Write.Print("\n[!] > Exiting...", Colors.red, interval = 0)
+            clear()
+            Write.Print("\n[!] > Exiting...", default_color, interval = 0)
             exit()
 
+        elif choice == "99":
+            settings()
+
         else:
-            os.system('cls' if os.name == 'nt' else 'clear')
-            Write.Print("[!] > Invalid input. Please enter a number from 1 to 4\n", Colors.red, interval = 0)
+            clear()
+            Write.Print("[!] > Invalid input.\n", default_color, interval = 0)
+            continue
+
+def settings():
+    clear()
+    while True:
+        settings_menu_art = """\n   ▄████████  ▄██████▄      ███     
+  ███    ███ ███    ███ ▀█████████▄ 
+  ███    █▀  ███    ███    ▀███▀▀██ 
+  ███        ███    ███     ███   ▀ 
+▀███████████ ███    ███     ███     
+         ███ ███    ███     ███     
+   ▄█    ███ ███    ███     ███     
+ ▄████████▀   ▀██████▀     ▄████▀   
+\n"""
+
+        settings_menu = """\n\n                                 [ S.O.T ]\n
+                             Simple OSINT Tools
+                                 Settings
+
+╭─    ─╮╭─                   ─╮╭─                                         ─╮
+|  №   ||       Setting       ||                Description                |
+|======||=====================||===========================================|
+| [1]  || Theme change        || Customize the SOT theme                   |
+| [?]  || In development...   || ???                                       |
+|------||---------------------||-------------------------------------------|
+| [0]  || Back to menu        || Exit the settings                         |
+╰─    ─╯╰─                   ─╯╰─                                         ─╯\n\n\n"""
+        Write.Print(Center.XCenter(settings_menu_art), default_color, interval = 0)
+        Write.Print(Center.XCenter(settings_menu), Colors.white, interval = 0)
+
+        settings_choice = Write.Input("[?] >  ", default_color, interval = 0).strip()
+
+        if settings_choice == "1":
+            change_color()
+        elif settings_choice == "0":
+            main()
+        else:
+            clear()
+            Write.Print("[!] > Invalid input.\n", default_color, interval = 0)
             continue
 
 main()
