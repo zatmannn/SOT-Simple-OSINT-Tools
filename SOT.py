c5658d71d87995ec066abe45c6c830b54d169dbb
zatman <yaeblannn22222@proton.me>
2024-12-18 11:13:53 +0700

    Update SOT.py

--- SOT.py
+++ SOT.py
@@ -1,18 +1,31 @@
 import requests
 from pystyle import Colors, Write, Center
-from phonenumbers import geocoder, carrier, timezone
+from phonenumbers import geocoder, carrier
 import phonenumbers
 import whois
 import os
 from concurrent.futures import ThreadPoolExecutor
+import re
 
 default_color = Colors.red  
+
+menu_art = """
+\n
+   ▄████████  ▄██████▄      ███     
+  ███    ███ ███    ███ ▀█████████▄ 
+  ███    █▀  ███    ███    ▀███▀▀██ 
+  ███        ███    ███     ███   ▀ 
+▀███████████ ███    ███     ███     
+         ███ ███    ███     ███     
+   ▄█    ███ ███    ███     ███     
+ ▄████████▀   ▀██████▀     ▄████▀   
+\n"""
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def restart():
-    Write.Input("\nPress Enter to return to the main menu...", default_color, interval=0)
+    Write.Input("\n\n(@SOT)─[!] > Press Enter to return to the main menu...", default_color, interval=0)
     clear()
 
 def ip_info(ip):
@@ -25,15 +38,15 @@ def ip_info(ip):
 ╭─{" "*78}─╮
 |{' '*34} IP Details {' '*34}|
 |{"="*80}|
-| [+] > IP Address         || {response.get('ip', 'None'): <51}|
-| [+] > City               || {response.get('city', 'None'): <51}|
-| [+] > Region             || {response.get('region', 'None'): <51}|
-| [+] > Country            || {response.get('country', 'None'): <51}|
-| [+] > Postal code        || {response.get('postal', 'None'): <51}|
-| [+] > ISP                || {response.get('org', 'None'): <51}|
-| [+] > Latitude, Longitude|| {response.get('loc', 'None'): <51}|
-| [+] > Timezone           || {response.get('timezone', 'None'): <51}|
-| [+] > Google Maps        || https://www.google.com/maps?q={response.get('loc', 'None'): <21}|
+| [+] > IP Address         || {response.get('ip') or "None": <51}|
+| [+] > City               || {response.get('city') or "None": <51}|
+| [+] > Region             || {response.get('region') or "None": <51}|
+| [+] > Country            || {response.get('country') or "None": <51}|
+| [+] > Postal code        || {response.get('postal') or "None": <51}|
+| [+] > ISP                || {response.get('org') or "None": <51}|
+| [+] > Latitude, Longitude|| {response.get('loc') or "None": <51}|
+| [+] > Timezone           || {response.get('timezone') or "None": <51}|
+| [+] > Google Maps        || https://www.google.com/maps?q={response.get('loc'): <21}|
 ╰─{" "*24}─╯╰─{" "*50}─╯
 """
         Write.Print(Center.XCenter(ip_details), Colors.white, interval=0)
@@ -52,6 +65,7 @@ def ip_info(ip):
         Write.Print(f"\n[!] > Error", default_color, interval = 0)
 
     restart()
+    return
 
 def username_search(nickname):
     def check_url(url):
@@ -105,13 +119,14 @@ def username_search(nickname):
     Write.Print(Center.XCenter(search_results), Colors.white, interval=0)
 
     restart()
+    return
 
 def phone_info(phone_number):
     try:
         parsed_number = phonenumbers.parse(phone_number)
-        country = geocoder.country_name_for_number(parsed_number, "en")
-        region = geocoder.description_for_number(parsed_number, "en")
-        operator = carrier.name_for_number(parsed_number, "en")
+        country = geocoder.country_name_for_number(parsed_number, "en") or "Unknown"
+        region = geocoder.description_for_number(parsed_number, "en") or "Unknown"
+        operator = carrier.name_for_number(parsed_number, "en") or "Unknown"
         valid = phonenumbers.is_valid_number(parsed_number)
         validity = "Valid" if valid else "Invalid"
         phonetext = f"""
@@ -124,15 +139,16 @@ def phone_info(phone_number):
 | [+] > Regiom   || {region: <33}|
 | [+] > Operator || {operator: <33}|
 | [+] > Validity || {validity: <33}|
-╰─{" "*15}─╯╰─{" "*31}─╯\n"""
+╰─{" "*14}─╯╰─{" "*32}─╯\n"""
 
         Write.Print(Center.XCenter(phonetext), Colors.white, interval=0)
     
     except phonenumbers.phonenumberutil.NumberParseException:
         clear()
-        Write.Print(f"\n[!] > Error: invalid phone number format", default_color, interval = 0)
+        Write.Print(f"\n(@SOT)─[!] > Error: invalid phone number format", default_color, interval = 0)
 
     restart()
+    return
 
 def domain_info(domain):
 
@@ -148,27 +164,131 @@ def domain_info(domain):
         domain_creation_date = domain_date(domain_data.creation_date)
         domain_expiration_date = domain_date(domain_data.expiration_date)
         domain_updated_date = domain_date(domain_data.updated_date)
-        registrant_name = domain_data.registrant_name if domain_data.registrant_name else "Private"
-        registrant_email = domain_data.registrant_email if domain_data.registrant_email else "Private"
+        registrant_name = domain_data.registrant_name or "Private"
+        registrant_email = domain_data.registrant_email or "Private"
 
         domain_details = f"""
 ╭─{" "*78}─╮
 |{' '*34} WHOIS Info {' '*34}|
 |{"="*80}|
-| [+] > Domain:           || {domain_name: <52}|
-| [+] > Registrar:        || {domain_registrar: <52}|
-| [+] > Creation Date:    || {domain_creation_date: <52}|
-| [+] > Expiration Date:  || {domain_expiration_date: <52}|
-| [+] > Last Updated:     || {domain_updated_date: <52}|
-| [+] > Registrant Name:  || {registrant_name: <52}|
-| [+] > Registrant Email: || {registrant_email: <52}|
-╰─{" "*23}─╯╰─{" "*51}─╯
+| [+] > Domain           || {domain_name: <53}|
+| [+] > Registrar        || {domain_registrar: <53}|
+| [+] > Creation Date    || {domain_creation_date: <53}|
+| [+] > Expiration Date  || {domain_expiration_date: <53}|
+| [+] > Last Updated     || {domain_updated_date: <53}|
+| [+] > Registrant Name  || {registrant_name: <53}|
+| [+] > Registrant Email || {registrant_email: <53}|
+╰─{" "*22}─╯╰─{" "*52}─╯
 """
         Write.Print(Center.XCenter(domain_details), Colors.white, interval=0)
-    except Exception:
-        Write.Print(f"[!] > Error", default_color, interval=0)
+    except Exception as e:
+        Write.Print(f"\n(@SOT)─[!] > Error: {e}", default_color, interval=0)
 
     restart()
+
+def server_lookup(invite_link):
+
+    def clear_emoji(names):
+        return re.sub(r'[^\w\s]', '', names)
+
+    try:
+        response = requests.get(f"https://discord.com/api/v9/invites/{invite_link}")
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        Write.Print("\n(@SOT)─[-] > Request failed. Either Discord servers are down, or the link is invalid.\n", default_color, interval=0)
+        restart()
+        return
+    except Exception as e:
+        Write.Print(f"\n(@SOT)─[-] > Error: {str(e)}\n", default_color, interval=0)
+        restart()
+        return
+
+    if response.status_code == 200:
+        discord = response.json()
+        inviter = discord.get('inviter', {})
+        username = inviter.get('username') or 'Unknown'
+        userid = inviter.get('id') or 'Unknown'
+        guild = discord.get('guild', {})
+        code = discord.get('code')
+        date = discord.get('expires_at') or 'Never'
+        serverid = guild.get('id') or 'Unknown'
+        verification = guild.get('verification_level') or 'Unknown'
+        channel = clear_emoji(discord.get('channel', {}).get('name') or 'Unknown')
+        servername = clear_emoji(guild.get('name') or 'Unknown')
+
+        results = f"""
+╭─{" "*76}─╮
+|{' '*32}Invite Details{' '*32}|
+|{"="*78}|
+| [+] > Invite Link         || https://discord.gg/{code:<29}|
+| [+] > Target Channel      || {channel:<48}|
+| [+] > Expiration Date     || {date:<48}|
+|{"="*78}|
+|{' '*31}Inviter Details{' '*32}|
+|{"="*78}|
+| [+] > Username            || {username:<48}|
+| [+] > User ID             || {userid:<48}|
+|{"="*78}|
+|{' '*32}Server Details{' '*32}|
+|{"="*78}|
+| [+] > Server Name         || {servername:<48}|
+| [+] > Server ID           || {serverid:<48}|
+| [+] > Verification Level  || {verification:<48}|
+╰─{" "*25}─╯╰─{" "*47}─╯
+"""
+        Write.Print(Center.XCenter(results), Colors.white, interval=0)
+    restart()
+    return
+
+def token_info(token):
+
+    headers = {'Authorization': token,
+               'Content-Type': 'application/json'}
+
+    try:
+        response = requests.get('https://discord.com/api/v10/users/@me', headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as http_err:
+        Write.Print(f"\n(@SOT)─[-] > HTTP Error: {http_err.response.status_code}\n", default_color, interval=0)
+        restart()
+        return
+    except requests.exceptions.RequestException:
+        Write.Print("\n(@SOT)─[-] > Request failed. Either Discord servers are down, or the token is invalid.\n", default_color, interval=0)
+        restart()
+    except Exception as e:
+        Write.Print(f"\n(@SOT)─[-] > Error: {str(e)}\n", default_color, interval=0)
+        restart()
+        return
+
+    if response.status_code == 200:
+
+        discord = response.json()
+        nickname = discord["username"]
+        userid = discord["id"]
+        token_email = discord["email"] or "Unknown"
+        phonenumber = discord["phone"] or "Unknown"
+        mfa = discord['mfa_enabled'] or "None"
+        nitro_status = discord['premium_type']
+        nitro = "Yes" if nitro_status else "No"
+        verify = discord['verified']
+        verified = "Yes" if verify else "No"
+
+    results = f"""
+╭─{" "*76}─╮
+|{' '*32}Token Details{' '*33}|
+|{"="*78}|
+| [+] > Username          || {nickname:<50}|
+| [+] > User ID           || {userid:<50}|
+| [+] > Email             || {token_email:<50}|
+| [+] > Phone number      || {phonenumber:<50}|
+| [+] > Nitro             || {nitro:<50}|
+| [+] > MFA               || {mfa:<50}|
+| [+] > Email verified    || {verified:<50}|
+╰─{" "*23}─╯╰─{" "*49}─╯"""
+
+    Write.Print(Center.XCenter(results), Colors.white, interval=0)
+    restart()
+    return
 
 def change_color():
     global default_color
@@ -189,7 +309,7 @@ def change_color():
 """
     Write.Print(Center.XCenter(color_menu), Colors.white, interval=0)
 
-    color_choice = Write.Input("\n\n[?] >  ", default_color, interval=0).strip()
+    color_choice = Write.Input("\n\n(@SOT)─[?] >  ", default_color, interval=0).strip()
 
     color = {"1": Colors.red,
         "2": Colors.blue,
@@ -201,89 +321,101 @@ def change_color():
     if color_choice in color:
         default_color = color[color_choice]
         clear()
-        Write.Print("[!] > Color changed successfully.\n", default_color, interval=0)
+        Write.Print("(@SOT)─[!] > Color changed successfully.\n", default_color, interval=0)
     elif color_choice == "0":
         settings()
     else:
         clear()
-        Write.Print("[!] > Invalid choice. Please choose a number from 1 to 6.\n", Colors.red, interval=0)
+        Write.Print("(@SOT)─[!] > Invalid choice. Please choose a number from 1 to 6.\n", Colors.red, interval=0)
     restart()
+    return
+
 
 def main():
     clear()
     while True:
-        menu_art = """\n   ▄████████  ▄██████▄      ███     
-  ███    ███ ███    ███ ▀█████████▄ 
-  ███    █▀  ███    ███    ▀███▀▀██ 
-  ███        ███    ███     ███   ▀ 
-▀███████████ ███    ███     ███     
-         ███ ███    ███     ███     
-   ▄█    ███ ███    ███     ███     
- ▄████████▀   ▀██████▀     ▄████▀   
-\n"""
-
         menu = """\n\n                                   [ S.O.T ]\n
                                Simple OSINT Tools
 
-╭─    ─╮╭─                   ─╮╭─                                             ─╮
-|  №   ||      Function       ||                  Description                  |
-|======||=====================||===============================================|
-| [1]  || IP Info Search      || Search for information about an IP address    |
-| [2]  || Username Search     || Search for profiles on social media platforms |
-| [3]  || Phone Info Search   || Search for information about a phone number   |
-| [4]  || Domain Info Search  || Search for information about a domain         |
-|------||---------------------||-----------------------------------------------|
-| [0]  || Exit                || Exit the program                              |
-| [99] || Settings            || Set up this tool                              |
-╰─    ─╯╰─                   ─╯╰─                                             ─╯\n\n\n"""
+╭─    ─╮╭─                     ─╮╭─                                             ─╮
+|  №   ||       Function        ||                  Description                  |
+|======||=======================||===============================================|
+| [1]  || IP Info Search        || Search for information about an IP address    |
+| [2]  || Username Search       || Search for profiles on social media platforms |
+| [3]  || Phone Info Search     || Search for information about a phone number   |
+| [4]  || Domain Info Search    || Search for information about a domain         |
+| [5]  || Discord invite lookup || Search for information about a Discord invite |
+| [6]  || Discord token info    || Search for information about a Discord token  |
+|------||-----------------------||-----------------------------------------------|
+| [0]  || Exit                  || Exit the program                              |
+| [99] || Settings              || Set up this tool                              |
+╰─    ─╯╰─                     ─╯╰─                                             ─╯\n\n\n"""
 
         Write.Print(Center.XCenter(menu_art), default_color, interval = 0)
         Write.Print(Center.XCenter(menu), Colors.white, interval = 0)
 
-        choice = Write.Input("[?] >  ", default_color, interval = 0).strip()
+        choice = Write.Input("(@SOT)─[?] > ", default_color, interval = 0)
 
         if choice == "1":
             clear()
-            ip = Write.Input("[?] > IP-Adress: ", default_color, interval = 0)
+            ip = Write.Input("(@SOT)─[?] > IP-Adress: ", default_color, interval = 0)
             if not ip:
                 clear()
-                Write.Print("[!] > Please, enter IP Adress\n", default_color, interval = 0)
+                Write.Print("(@SOT)─[!] > Please, enter IP Adress\n", default_color, interval = 0)
                 continue
             ip_info(ip)
 
         elif choice == "2":
             clear()
-            nickname = Write.Input("[?] > Username: ", default_color, interval = 0)
-            Write.Print("[!] > Please wait, it may take some time...\n", default_color, interval = 0)
+            nickname = Write.Input("(@SOT)─[?] > Username: ", default_color, interval = 0)
+            Write.Print("(@SOT)─[!] > Please wait, it may take some time...\n", default_color, interval = 0)
             if not nickname:
                 clear()
-                Write.Print("[!] > Please, enter username\n", default_color, interval = 0)
+                Write.Print("(@SOT)─[!] > Please, enter username\n", default_color, interval = 0)
                 continue
             username_search(nickname)
 
         elif choice == "3":
             clear()
-            phone_number = Write.Input("[?] > Phone number: ", default_color, interval = 0)
+            phone_number = Write.Input("(@SOT)─[?] > Phone number: ", default_color, interval = 0)
             if not phone_number:
                 clear()
-                Write.Print("[!] > Please, enter phone number\n", default_color, interval = 0)
+                Write.Print("(@SOT)─[!] > Please, enter phone number\n", default_color, interval = 0)
                 continue
             phone_info(phone_number)
             
         elif choice == "4":
             clear()
-            domain = Write.Input("[?] > URL: ", default_color, interval = 0)
-            Write.Print("[!] > Please wait, it may take some time...\n", default_color, interval = 0)
+            domain = Write.Input("(@SOT)─[?] > URL: ", default_color, interval = 0)
+            Write.Print("(@SOT)─[!] > Please wait, it may take some time...\n", default_color, interval = 0)
             if not domain:
                 clear()
-                Write.Print("[!] > Please, enter URL\n", default_color, interval = 0)
+                Write.Print("(@SOT)─[!] > Please, enter URL\n", default_color, interval = 0)
                 continue
             domain_info(domain)
+
+        elif choice == "5":
+            clear()
+            invite_link = Write.Input("(@SOT)─[?] > Invite code: ", default_color, interval = 0)
+            if not invite_link:
+                clear()
+                Write.Print("(@SOT)─[!] > Please, enter invite code\n", default_color, interval = 0)
+                continue
+            server_lookup(invite_link)
+
+        elif choice == "6":
+            clear()
+            token = Write.Input("(@SOT)─[?] > Token: ", default_color, interval = 0)
+            if not token:
+                clear()
+                Write.Print("(@SOT)─[!] > Please, enter token\n", default_color, interval = 0)
+                continue
+            token_info(token)
 
 
         elif choice == "0":
             clear()
-            Write.Print("\n[!] > Exiting...", default_color, interval = 0)
+            Write.Print("\n(@SOT)─[!] > Exiting...\n", default_color, interval = 0)
             exit()
 
         elif choice == "99":
@@ -291,46 +423,37 @@ def main():
 
         else:
             clear()
-            Write.Print("[!] > Invalid input.\n", default_color, interval = 0)
+            Write.Print("(@SOT)─[!] > Invalid input\n", default_color, interval = 0)
             continue
 
 def settings():
     clear()
     while True:
-        settings_menu_art = """\n   ▄████████  ▄██████▄      ███     
-  ███    ███ ███    ███ ▀█████████▄ 
-  ███    █▀  ███    ███    ▀███▀▀██ 
-  ███        ███    ███     ███   ▀ 
-▀███████████ ███    ███     ███     
-         ███ ███    ███     ███     
-   ▄█    ███ ███    ███     ███     
- ▄████████▀   ▀██████▀     ▄████▀   
-\n"""
-
-        settings_menu = """\n\n                                 [ S.O.T ]\n
-                             Simple OSINT Tools
+        settings_menu = """\n\n                                [ S.O.T ]\n
+                            Simple OSINT Tools
                                  Settings
 
 ╭─    ─╮╭─                   ─╮╭─                                         ─╮
 |  №   ||       Setting       ||                Description                |
 |======||=====================||===========================================|
 | [1]  || Theme change        || Customize the SOT theme                   |
-| [?]  || In development...   || ???                                       |
+| [2]  || Language change     || Change the SOT language                   |
 |------||---------------------||-------------------------------------------|
 | [0]  || Back to menu        || Exit the settings                         |
 ╰─    ─╯╰─                   ─╯╰─                                         ─╯\n\n\n"""
-        Write.Print(Center.XCenter(settings_menu_art), default_color, interval = 0)
+        Write.Print(Center.XCenter(menu_art), default_color, interval = 0)
         Write.Print(Center.XCenter(settings_menu), Colors.white, interval = 0)
 
-        settings_choice = Write.Input("[?] >  ", default_color, interval = 0).strip()
+        settings_choice = Write.Input("(@SOT)─[?] >  ", default_color, interval = 0).strip()
 
         if settings_choice == "1":
             change_color()
+
         elif settings_choice == "0":
             main()
         else:
             clear()
-            Write.Print("[!] > Invalid input.\n", default_color, interval = 0)
+            Write.Print("(@SOT)─[!] > Invalid input.\n", default_color, interval = 0)
             continue
 
 main()
